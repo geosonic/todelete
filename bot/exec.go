686daaf2099e4aa3f82c1a8dcf92c678f74c01b7
geosonic <36260850+geosonic@users.noreply.github.com>
2020-03-06 13:12:50 +0500

    Оптимизация кода
    Убран лишний код

--- bot/exec.go
+++ bot/exec.go
@@ -12,76 +12,48 @@ Execute функции
 
 */
 
+const code = `
+// Количество которое необходимо удалить
+// Эти переменные устанавливаются скриптом!!!
+var toDeleteCount = %v; // int
+var peer_id = %v; // int
+		
+// Переменная отсортированных сообщений
+var toDelete = [];
+		
+// Получаем сообщения
+var resp = API.messages.getHistory({peer_id: peer_id, count: 200});
+// Получаем ID аккаунта
+var myID = API.users.get()[0].id;
+		
+// Количество элементов для цикла
+var count = resp.items.length;
+// Счётчик для цикла
+var counter = 0;
+		
+while (counter < count) {
+// Переменная сообщения
+var message = resp.items[counter];
+		
+if (message.from_id == myID && toDelete.length < toDeleteCount) {
+toDelete.push(message.id);
+}
+		
+// Итерация
+counter = counter + 1;
+}
+`
+
 func DeleteExec(vk *api.VK, toDeleteCount, peerID int) {
 	code :=
-		fmt.Sprintf(`// Количество которое необходимо удалить
-		// Эти переменные устанавливаются скриптом!!!
-		var toDeleteCount = %v; // int
-		var peer_id = %v; // int
-		
-		// Переменная отсортированных сообщений
-		var toDelete = [];
-		
-		// Получаем сообщения
-		var resp = API.messages.getHistory({peer_id: peer_id, count: 200});
-		// Получаем ID аккаунта
-		var myID = API.users.get()[0].id;
-		
-		// Количество элементов для цикла
-		var count = resp.items.length;
-		// Счётчик для цикла
-		var counter = 0;
-		
-		while (counter < count) {
-		// Переменная сообщения
-		var message = resp.items[counter];
-		
-		if (message.from_id == myID && toDelete.length < toDeleteCount) {
-		toDelete.push(message.id);
-		}
-		
-		// Итерация
-		counter = counter + 1;
-		}
-		
-		// Возвращаем результат
+		fmt.Sprintf(code + `// Возвращаем результат
 		return API.messages.delete({message_ids: toDelete, delete_for_all: 1});`, toDeleteCount, peerID)
 
 	_ = vk.Execute(code, nil)
 }
 
 func GetMessages(vk *api.VK, toDeleteCount, peerID int) []int {
-	code := fmt.Sprintf(`// Количество которое необходимо удалить
-		// Эти переменные устанавливаются скриптом!!!
-		var toDeleteCount = %v; // int
-		var peer_id = %v; // int
-
-		// Переменная отсортированных сообщений
-		var toDelete = [];
-
-		// Получаем сообщения
-		var resp = API.messages.getHistory({peer_id: peer_id, count: 200});
-		// Получаем ID аккаунта
-		var myID = API.users.get()[0].id;
-
-		// Количество элементов для цикла
-		var count = resp.items.length;
-		// Счётчик для цикла
-		var counter = 0;
-
-		while (counter < count) {
-		// Переменная сообщения
-		var message = resp.items[counter];
-
-		if (message.from_id == myID && toDelete.length < toDeleteCount) {
-		toDelete.push(message.id);
-		}
-
-		// Итерация
-		counter = counter + 1;
-		}
-
-		// Возвращаем результат
+	code := fmt.Sprintf(code + `// Возвращаем результат
 		return {messages: toDelete};`, toDeleteCount, peerID)
 
 	var resp struct {
