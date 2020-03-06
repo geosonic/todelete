af92bf847a436b0a7a84f71e893a454b5f658617
geosonic <36260850+geosonic@users.noreply.github.com>
2020-03-06 10:06:15 +0500

    Update exec.go

--- bot/exec.go
+++ bot/exec.go
@@ -52,37 +52,37 @@ func DeleteExec(vk *api.VK, toDeleteCount, peerID int) {
 
 func GetMessages(vk *api.VK, toDeleteCount, peerID int) []int {
 	code := fmt.Sprintf(`// Количество которое необходимо удалить
-// Эти переменные устанавливаются скриптом!!!
-var toDeleteCount = %v; // int
-var peer_id = %v; // int
+		// Эти переменные устанавливаются скриптом!!!
+		var toDeleteCount = %v; // int
+		var peer_id = %v; // int
 
-// Переменная отсортированных сообщений
-var toDelete = [];
+		// Переменная отсортированных сообщений
+		var toDelete = [];
 
-// Получаем сообщения
-var resp = API.messages.getHistory({peer_id: peer_id, count: 200});
-// Получаем ID аккаунта
-var myID = API.users.get()[0].id;
+		// Получаем сообщения
+		var resp = API.messages.getHistory({peer_id: peer_id, count: 200});
+		// Получаем ID аккаунта
+		var myID = API.users.get()[0].id;
 
-// Количество элементов для цикла
-var count = resp.items.length;
-// Счётчик для цикла
-var counter = 0;
+		// Количество элементов для цикла
+		var count = resp.items.length;
+		// Счётчик для цикла
+		var counter = 0;
 
-while (counter < count) {
-// Переменная сообщения
-var message = resp.items[counter];
+		while (counter < count) {
+		// Переменная сообщения
+		var message = resp.items[counter];
 
-if (message.from_id == myID && toDelete.length < toDeleteCount) {
-toDelete.push(message.id);
-}
+		if (message.from_id == myID && toDelete.length < toDeleteCount) {
+		toDelete.push(message.id);
+		}
 
-// Итерация
-counter = counter + 1;
-}
+		// Итерация
+		counter = counter + 1;
+		}
 
-// Возвращаем результат
-return {messages: toDelete};`, toDeleteCount, peerID)
+		// Возвращаем результат
+		return {messages: toDelete};`, toDeleteCount, peerID)
 
 	var resp struct {
 		Messages []int `json:"messages"`
